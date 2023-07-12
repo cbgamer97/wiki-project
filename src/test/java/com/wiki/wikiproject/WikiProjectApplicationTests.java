4ed8102feecd7107c0e6fb65e1897d27df6e899e
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-07-12 14:28:12 -0500

    setting up project

---
+++ src/test/java/com/wiki/wikiproject/WikiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiki.wikiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
