fbda4483eaf494712ac70572ca1a3ef56daf059e
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-07-13 13:53:23 -0500

    working on nav bar and some of the base styling for the site

--- src/main/java/com/wiki/wikiproject/controller/IndexController.java
+++ src/main/java/com/wiki/wikiproject/controller/IndexController.java
@@ -1,12 +1,12 @@
 package com.wiki.wikiproject.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home() {
         return "index";
     }
